f710f60bdd47f261bc04a0ec7d95debed8c62de5
Mikel <mikelon.aranburu@gmail.com>
2025-12-14 20:08:23 +0100

    fix

--- wallet.py
+++ wallet.py
@@ -1,6 +1,6 @@
 class Wallet:
-    def __init__(self):    # (self, balance):
-        self.balance = 10      #self.balance = balance
+    def __init__(self, balance):
+        self.balance = balance
 
     def set_balance(self, val):
         self.balance = self.balance + val
